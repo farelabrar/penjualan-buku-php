424abab95f8999d659aeab5b9d61fa1a313e1025
Farelabrar <abrarfarrel@gmail.com>
2024-10-16 21:03:44 +0700

    tambah my prfile

--- admin/index.php
+++ admin/index.php
@@ -49,7 +49,7 @@ function rupiah($nilai)
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">  <span><?php echo $_SESSION['nama']  ?></span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
-								<li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
+								<li><a href="my_profile.php"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
 								<li><a href="#"><i class="lnr lnr-envelope"></i> <span>Message</span></a></li>
 								<li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
 								<li><a href="logout.php"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
